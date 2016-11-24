7cfa79c683be9487819368de1082552df08f19dc
David Arteaga <david17art@gmail.com>
2016-11-24 16:25:53 -0400

    ajax parte 2
    ajax con el metodo post

--- ajax/controller.js
+++ ajax/controller.js
@@ -1,6 +1,7 @@
 var app = angular.module("MyFirstApp",[]);
 app.controller("FirstController",function($scope,$http){
   $scope.posts = [];
+  $scope.newPost = {};
   $http.get("https://jsonplaceholder.typicode.com/posts")
       .success(function(data){
         $scope.posts = data;
@@ -8,4 +9,18 @@ app.controller("FirstController",function($scope,$http){
       .error(function(){
         console.log(data);
       })
+      $scope.addPost = function(){
+        $http.post("https://jsonplaceholder.typicode.com/posts",{
+          tittle: $scope.newPost.title,
+          body: $scope.newPost.body,
+          userId: 1
+        })
+        .success(function(){
+          $scope.posts.push($scope.newPost);
+          $scope.newPost = {};
+        })
+        .error(function(error,status,headers,config){
+          console.log(error);
+        });
+      }
 });
