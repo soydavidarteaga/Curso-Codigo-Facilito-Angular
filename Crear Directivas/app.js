04c9ba3179f9bc7af58b1082b1bbfa28c0b2d72d
David Arteaga <david17art@gmail.com>
2016-11-25 21:46:31 -0400

    Directivas 2
    Aqui hacemos un jquery autocomplete

--- Crear Directivas/app.js
+++ Crear Directivas/app.js
@@ -1,13 +1,33 @@
 angular.module("CustomDirective",[])
+.directive("myAutocomplete",function(){
+  function link(scope,element,attrs){
+      $(element).autocomplete({
+        source: scope.$eval[attrs.myAutocomplete],
+        select: function(ev,ui){
+            ev.preventDefault();
+            if(ui.item){
+              scope.optionSelected(ui.item.value);
+            }
+        },
+        focos: function(ev,ui){
+          ev.preventDefault();
+          $(this).val(ui.item.label);
+        }
+      });
+  };
+  return {
+    link: link
+  };
+})
 .directive('backImg',function(){
   return function($scope,element,attrs){
     attrs.backImg //En caso de que no sea js lo que esta adentro
     //Para ello se usa observe
     attrs.$observe('backImg',function(value){
       element.css({
-        "background": "url("+value+")",
-        "background-size": "cover",
-        "background-position": "center"
+        'background-image': "url('"+value+"')",
+        'background-size': 'cover',
+        'background-position': 'center'
 
       });
     });
@@ -15,12 +35,21 @@ angular.module("CustomDirective",[])
 })
 .controller("AppCtrl",function($scope,$http){
   $scope.repos = [];
-  $http.get("https://api.github.com/users/codigofacilito/repos")
+  $http.get("https://api.github.com/users/twitter/repos")
   .success(function(data){
-    console.log(data);
-    $scope.repos = data;
+    $scope.posts = data;
+    for (var i = data.length - 1; i >= 0; i--){
+      var repo = data[i];
+      $scope.repos.push(repo.name);
+    };
+    console.log($scope.repos)
   })
   .error(function(err){
     console.log(err);
   });
+  $scope.optionSelected = function(data){
+    $scope.$apply(function(){
+      $scope.main_repo = data;
+    })
+  }
 });
