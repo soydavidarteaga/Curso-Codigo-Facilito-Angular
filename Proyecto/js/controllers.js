ba6bfc34d5d7f69ce276786a802f7d686d307072
David Arteaga <david17art@gmail.com>
2016-11-26 01:19:22 -0400

    PROYECTO POSTAPP 2 Parte
    Aqui se efectura la segunda parte, muestra de post y de users

--- Proyecto/js/controllers.js
+++ Proyecto/js/controllers.js
@@ -1,4 +1,7 @@
 angular.module("FinalApp")
-.controller("MainController",function($scope){
-  
-})
+.controller("MainController",function($scope,$resource){
+  Post = $resource("https://jsonplaceholder.typicode.com/posts/:id",{id: "@id"});
+  User = $resource("https://jsonplaceholder.typicode.com/users/:id",{id: "@id"})
+  $scope.posts = Post.query();
+  $scope.users = User.query();
+});
