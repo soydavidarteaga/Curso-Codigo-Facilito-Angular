41722caa830652070e832e5f6578d8ad123cf784
David Arteaga <david17art@gmail.com>
2016-11-26 22:13:28 -0400

    POSTAPP Parte 4
    Aprendimos  a  editar y agregar nuevos post, asi mismo a redirigirnos
    con location

--- Proyecto/js/controllers.js
+++ Proyecto/js/controllers.js
@@ -1,11 +1,10 @@
 angular.module("FinalApp")
-.controller("MainController",function($scope,$resource){
-  Post = $resource("https://jsonplaceholder.typicode.com/posts/:id",{id: "@id"});
+.controller("MainController",function($scope,$resource,PostResource){
   User = $resource("https://jsonplaceholder.typicode.com/users/:id",{id: "@id"});
-  $scope.posts = Post.query();
+  $scope.posts = PostResource.query();
   $scope.users = User.query();
   $scope.removePost = function(post){
-    Post.delete({id: post.id}, function(data){
+    PostResource.delete({id: post.id}, function(data){
       console.log(data);
     });
     $scope.posts = $scope.posts.filter(function(element){
@@ -13,7 +12,23 @@ angular.module("FinalApp")
     });
   }
 })
-.controller("PostController",function($scope,$resource,$routeParams){
-  Post = $resource("https://jsonplaceholder.typicode.com/posts/:id",{id: "@id"});
-  $scope.post = Post.get({id: $routeParams.id});
+.controller("PostController",function($scope,PostResource,$routeParams,$location){
+  $scope.title = "Editar Post";
+  $scope.post = PostResource.get({id: $routeParams.id});
+  $scope.savePost = function(){
+    PostResource.update({id: $scope.post.id},{data: $scope.post},function(data){
+      console.log(data);
+      $location.path("/post/"+$scope.post.id);
+    });
+  }
+})
+.controller("NewPostController",function($scope,PostResource,$location){
+  $scope.post = {};
+  $scope.title = "Crear Post";
+  $scope.savePost = function(){
+    PostResource.save({data: $scope.post},function(data){
+      console.log(data);
+      $location.path("/");
+    });
+  }
 });
